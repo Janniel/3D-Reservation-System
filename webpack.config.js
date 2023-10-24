877d82579b4653c2408f33739b0037bafa52dc85
Janniel Andrei De Jesus <jianbird.strelizia@gmail.com>
2023-10-25 04:26:27 +0800

    fix configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: {
         reserve: path.resolve(__dirname, 'src/reserve.js'),
+        home: path.resolve(__dirname, 'src/home.js'),
+        clock: path.resolve(__dirname, 'src/clock.js'),
+        login: path.resolve(__dirname, 'src/login.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
     },
 
     devtool: 'source-map',
@@ -17,11 +27,11 @@ module.exports = {
         static: {
             directory: path.resolve(__dirname, 'dist')
         },
-        port: 3000,
         open: true,
         hot: true,
         compress: true,
         historyApiFallback: true,
+        
     },
     
     module: {
@@ -54,14 +64,94 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
-            }
+            },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            //     type: 'asset/resource',
+            //     use: {
+            //         loader: 'file-loader',
+            //         options: {
+            //                 name: '[name].[ext]',
+            //                 outputPath: 'img'
+            //         }
+            //     },
+            // },
         ]
     },
     plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                  from: 'src/img',
+                  to: 'img',
+                },
+              ],
+        }),
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: 'src/reserve.html'
-        })
+            inject: true,
+            filename: 'reserve.php',
+            template: 'src/reserve.php',
+            chunks: ['reserve']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'index.php',
+            template: 'src/index.php',
+            chunks: ['home','clock']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'login.php',
+            template: 'src/login.php',
+            chunks: ['login']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/connect.php',
+            template: 'src/php/connect.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/session.php',
+            template: 'src/php/session.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/get_server_time.php',
+            template: 'src/php/get_server_time.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/header_notLogged.php',
+            template: 'src/php/header_notLogged.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/header.php',
+            template: 'src/php/header.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/occupancy_timer.php',
+            template: 'src/php/occupancy_timer.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/check_user_id.php',
+            template: 'src/php/check_user_id.php',
+            chunks: ['']
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'php/login_modal.php',
+            template: 'src/php/login_modal.php',
+            chunks: ['']
+        }),
     ]
 }
