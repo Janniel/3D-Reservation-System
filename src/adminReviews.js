02521936b6043de734f9f34f7e1e2cb278dc48e3
queenyxies <queenycasaclang@gmail.com>
2023-11-05 12:26:07 +0800

    admin reviews fixed

--- src/adminReviews.js
+++ src/adminReviews.js
@@ -1,1 +1,14 @@
 import "./styles/adminReviews.css"
+
+
+const progressDone = document.querySelectorAll('.progress-done');
+
+progressDone.forEach(progress => {
+	progress.style.width = progress.getAttribute('data-done') + '%';
+});
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
