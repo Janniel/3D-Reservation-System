690b407abfec89c68198b3ba25975765d3c7f7a8
Ryka Gene Austria <rykastr@gmail.com>
2023-11-05 21:46:36 +0800

    added changes

--- src/php/header.php
+++ src/php/header.php
@@ -1,5 +1,5 @@
 <header class="header-outer">
-<div class="header-inner responsive-wrapper">
+  <div class="header-inner responsive-wrapper">
     <div class="header-logo">
       <img src="img/elib logo.png" class="icon">
     </div>
@@ -7,23 +7,23 @@
       <strong>SOAR</strong>
     </div>
     <div id="header-time">
-    <div id="phtime">
-      <p class="phdate">Philippine Time: </p>
-      <div id="date"></div>
-    </div>
-    <div id="clock">
-      <div class="clock">
-        <div class="hour">
-          <div class="hr" id="hr"></div>
-        </div>
-        <div class="min">
-          <div class="mn" id="mn"></div>
-        </div>
-        <div class="sec">
-          <div class="sc" id="sc"></div>
+      <div id="phtime">
+        <p class="phdate">Philippine Time: </p>
+        <div id="date"></div>
+      </div>
+      <div id="clock">
+        <div class="clock">
+          <div class="hour">
+            <div class="hr" id="hr"></div>
+          </div>
+          <div class="min">
+            <div class="mn" id="mn"></div>
+          </div>
+          <div class="sec">
+            <div class="sc" id="sc"></div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
     <nav class="header-navigation">
       <a class="active" href="home.php">HOME</a>
@@ -34,5 +34,5 @@
       <a id="hidden" href="toLogout.php">LOGOUT</a>
       <!-- <a id="show" href="login.php" >LOGIN</a> -->
     </nav>
-</div>
+  </div>
 </header>
