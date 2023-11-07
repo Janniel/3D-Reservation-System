4f8a79febb42940389842961ea27ee49c0576565
queenyxies <queenycasaclang@gmail.com>
2023-11-08 00:25:49 +0800

    update admin name in header

--- src/admin.php
+++ src/admin.php
@@ -162,16 +162,10 @@ $result = $conn->query($sql);
                     <button class="dropdown-toggle" class="btn btn-secondary dropdown-toggle" type="button"
                         id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                         <div class="user-wrapper">
-                            <img src="<?php if ($_SESSION['gender'] == "Male") {
-                                echo "https://cdn-icons-png.flaticon.com/512/2552/2552801.png";
-                            } elseif ($_SESSION['gender'] == "Female") {
-                                echo "https://cdn-icons-png.flaticon.com/512/206/206864.png";
-                            } ?>" alt="Admin" class="rounded-circle p-1 bg-secondary" width="45">
-                            <div id="user_admin">
-                                <h4>
-                                    <?php echo $_SESSION["username"]; ?>
-                                </h4>
-                            </div>
+                            <h4>
+                                Hello, <?php echo $_SESSION["first_name"]; ?>
+                            </h4>
+                            
                         </div>
                     </button>
 
